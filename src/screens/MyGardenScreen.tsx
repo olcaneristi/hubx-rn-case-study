d0a6fa5339171b78b3c7ccffc921cc4cd7a384fe
olcaneristi <ogulcaneristi123@gmail.com>
2025-04-27 16:29:39 +0300

    feat: add bottom tab screens basis

---
+++ src/screens/MyGardenScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const MyGardenScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>MyGardenScreen</Text>
+    </View>
+  );
+};
+
+export default MyGardenScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/ScanPlantScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const ScanPlantScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>ScanPlantScreen</Text>
+    </View>
+  );
+};
+
+export default ScanPlantScreen;
+
+const styles = StyleSheet.create({});
